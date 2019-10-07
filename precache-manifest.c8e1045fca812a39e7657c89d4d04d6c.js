a9cdebdd7d8ff63958a24622685a4e4689bce69b
abc <abc@example.com>
2019-10-07 17:03:24 +0300

    Updates

---
+++ precache-manifest.c8e1045fca812a39e7657c89d4d04d6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67065a04f00f2d541632959ce45840a",
+    "url": "/RecipeReact/index.html"
+  },
+  {
+    "revision": "db525b33d408bc327de3",
+    "url": "/RecipeReact/static/css/main.ffbf9a18.chunk.css"
+  },
+  {
+    "revision": "2a185c3e15e0571ed4a6",
+    "url": "/RecipeReact/static/js/2.775a5e75.chunk.js"
+  },
+  {
+    "revision": "db525b33d408bc327de3",
+    "url": "/RecipeReact/static/js/main.56d51bfc.chunk.js"
+  },
+  {
+    "revision": "45ffa3554bef39ba21fb",
+    "url": "/RecipeReact/static/js/runtime-main.2d92df50.js"
+  }
+]);
